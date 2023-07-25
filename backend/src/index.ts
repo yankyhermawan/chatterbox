666e0c542b9f82308bb5050b967ce521d432e772
yankyhermawan <yankyhermawan42@gmail.com>
2023-07-25 21:35:23 +0700

    Init Backend

---
+++ backend/src/index.ts
@@ -0,0 +1,15 @@
+import express from "express";
+import { createServer } from "http";
+import { Server } from "socket.io";
+
+const app = express();
+const server = createServer(app);
+const io = new Server(server);
+
+io.on("connection", (socket) => {
+	socket.on("chat message", (msg) => {
+		io.emit("chat message", msg);
+	});
+});
+
+server.listen(4000, () => console.log("listening on *:4000"));
