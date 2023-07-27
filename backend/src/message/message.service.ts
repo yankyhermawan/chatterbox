7a6da3c5a0dec6b9e66bbeaf746c145ff929635f
yankyhermawan <yankyhermawan42@gmail.com>
2023-07-27 20:32:43 +0700

    create-channel-endpoint

--- backend/src/message/message.service.ts
+++ backend/src/message/message.service.ts
@@ -2,29 +2,6 @@ import { PrismaService } from "../prisma.service";
 
 export class MessageService {
 	constructor(private readonly prismaService: PrismaService) {}
-
-	async getChannelMessage(roomID: string) {
-		try {
-			const response = await this.prismaService.message.findMany({
-				where: {
-					roomID: roomID,
-				},
-				orderBy: {
-					date: "desc",
-				},
-				take: 100,
-			});
-			return {
-				code: 200,
-				response: response,
-			};
-		} catch (err) {
-			return {
-				code: 500,
-				response: "Server error",
-			};
-		}
-	}
 	async postMessage(roomID: string, message: string, senderID: string) {
 		try {
 			const dataToPost = {
