d2fdc0a5588a6f4c4570f8ebf538349c2a77ce1c
Nurdin Beta <nurdinbeta@gmail.com>
2023-07-27 13:40:08 +0800

    add auth in register and login page

--- frontend/src/pages/components/FormInput.tsx
+++ frontend/src/pages/components/FormInput.tsx
@@ -1,19 +1,16 @@
 import React, { ChangeEvent } from 'react';
-import { IconType } from 'react-icons';
 
 interface FormInputProps {
   placeholder: string;
   type: string;
   name: string;
   value: string;
-  icon: IconType;
   onChange: (event: ChangeEvent<HTMLInputElement>) => void;
 }
 
-const FormInput: React.FC<FormInputProps> = ({ placeholder, type, name, value, icon: Icon, onChange }) => {
+const FormInput: React.FC<FormInputProps> = ({ placeholder, type, name, value, onChange }) => {
   return (
     <div className='relative py-2 text-center'>
-      <Icon className='absolute'/>
       <input className='border rounded-md w-full' type={type} name={name} value={value} placeholder={placeholder} onChange={onChange} />
     </div>
   );
