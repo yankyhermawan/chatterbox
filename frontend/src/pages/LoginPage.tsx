c2ce3d544c3185a342f963b5f2529249d7ce7ed3
nikosetiawanp <nikosetiawanp@gmail.com>
2023-07-28 10:06:55 +0700

    fixed login and register styling

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -1,9 +1,12 @@
-import { Link, useNavigate } from 'react-router-dom';
-import * as yup from 'yup';
-import { Controller, useForm } from 'react-hook-form';
-import { yupResolver } from '@hookform/resolvers/yup';
-import axios from 'axios';
-import { useState } from 'react';
+import { Link, useNavigate } from "react-router-dom";
+import * as yup from "yup";
+import { Controller, useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import axios from "axios";
+import { useState } from "react";
+import IconMail from "../assets/icon-mail.svg";
+import IconLock from "../assets/icon-lock.svg";
+import IconUserLogin from "../assets/icon-user-login.svg";
 
 interface LoginProps {
   email: string;
@@ -14,9 +17,8 @@ const schema = yup
   .object({
     email: yup.string().email().required(),
     password: yup.string().min(5).required(),
-    })
+  })
   .required();
-
 
 function LoginPage() {
   const [submitLogin, setSubmitLogin] = useState(false);
@@ -29,96 +31,113 @@ function LoginPage() {
     resolver: yupResolver(schema),
   });
 
-   async function onSubmit(formData: LoginProps) {
+  async function onSubmit(formData: LoginProps) {
     setSubmitLogin(true);
     try {
-      const response = await axios.post('https://w24-group-final-group-3-production.up.railway.app/login', {
-        email: formData.email,
-        password: formData.password,
-      });
-    
-      localStorage.setItem('token', response.data.data.token);
+      const response = await axios.post(
+        "https://w24-group-final-group-3-production.up.railway.app/login",
+        {
+          email: formData.email,
+          password: formData.password,
+        }
+      );
 
-      navigate('/');
+      localStorage.setItem("token", response.data.data.token);
+
+      navigate("/");
     } catch (error) {
       console.error(error);
     }
   }
- return (
-    <section className='bg-medium-grey'>
-      <div className='flex flex-col items-center justify-center px-6 py-8 mx-auto w-screen h-screen md:h-screen lg:py-0'>
-        <div className='w-2/4 bg-medium-grey overflow-hidden rounded-lg shadow border mt-0 max-w-md p-0'>
-          <div className='p-6 space-y-4'>
-            <h1 className='text-body-bold text-white text-center'>
-              Login 
-            </h1>
-            <form onSubmit={handleSubmit(onSubmit)} className='space-y-4 md:space-y-6' action='#'>
-              <div>
-                <Controller
-                  name='email'
-                  control={control}
-                  render={({ field }) => (
-                    <>
-                      <input
-                        type='text'
-                        name='email'
-                        id='email'
-                        value={field.value}
-                        onChange={field.onChange}
-                        className='bg-white border-gray-300 text-black sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 '
-                        placeholder='Email'
-                      />
-                      {errors?.email && (
-                        <p className='mt-2 text-sm text-red'>
-                          {errors.email.message}
-                        </p>
-                      )}
-                    </>
-                  )}
+  return (
+    <section className="flex justify-center items-center bg-medium-grey w-screen h-screen fixed top-0 left-0">
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="flex flex-col items-center space-y-4 md:space-y-6 w-full p-8 md:border border-text-grey rounded-xl max-w-[425px]"
+        action="#"
+      >
+        <h1 className="text-body-bold text-white text-center mb-4">Login</h1>
+        <div className="relative w-full flex flex-col items-start">
+          <img
+            className="absolute w-[20px] top-3 left-3"
+            src={IconMail}
+            alt="icon-mail"
+          />
+          <Controller
+            name="email"
+            control={control}
+            render={({ field }) => (
+              <>
+                <input
+                  type="text"
+                  name="email"
+                  id="email"
+                  value={field.value}
+                  onChange={field.onChange}
+                  className={`bg-medium-grey border-gray-300 pl-12 text-white sm:text-sm border outline-none border-text-grey ${
+                    errors?.email && "border-red"
+                  } rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5`}
+                  placeholder="Email"
                 />
-              </div>
-
-              <div>
-                <Controller
-                  name='password'
-                  control={control}
-                  render={({ field }) => (
-                    <>
-                      <input
-                        type='password'
-                        name='password'
-                        id='password'
-                        value={field.value}
-                        onChange={field.onChange}
-                        placeholder='Password'
-                        className='bg-white border-gray-300 text-black sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 '
-                      />
-                      {errors?.password && (
-                        <p className='mt-2 text-sm text-red'>
-                          {errors.password.message}
-                        </p>
-                      )}
-                    </>
-                  )}
-                />
-              </div>
-              <button
-                type='submit'
-                disabled={submitLogin}
-                className='w-full text-white bg-blue rounded-lg text-sm px-5 py-2.5 text-center'
-                onClick={handleSubmit(onSubmit)}>Login</button>
-              <p className='text-sm font-light text-center text-white'>
-                Not a member ? {" "}
-                <Link
-                  to='/register'
-                  className='font-medium mx-1 text-white text-primary-500 hover:underline '>
-                  Register
-                </Link>
-              </p>
-            </form>
-          </div>
+                {errors?.email && (
+                  <p className="text-sm text-red text-input-medium">
+                    {errors.email.message}
+                  </p>
+                )}
+              </>
+            )}
+          />
         </div>
-      </div>
+
+        <div className="relative w-full flex flex-col items-start">
+          <img
+            className="absolute w-[20px] top-3 left-3"
+            src={IconLock}
+            alt="icon-lock"
+          />
+          <Controller
+            name="password"
+            control={control}
+            render={({ field }) => (
+              <>
+                <input
+                  type="password"
+                  name="password"
+                  id="password"
+                  value={field.value}
+                  onChange={field.onChange}
+                  placeholder="Password"
+                  className={`bg-medium-grey border-gray-300 pl-12 text-white sm:text-sm border outline-none border-text-grey ${
+                    errors?.password && "border-red"
+                  } rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5`}
+                />
+                {errors?.password && (
+                  <p className="text-sm text-red text-input-medium">
+                    {errors.password.message}
+                  </p>
+                )}
+              </>
+            )}
+          />
+        </div>
+        <button
+          type="submit"
+          disabled={submitLogin}
+          className="w-full text-white text-body-medium bg-blue rounded-lg text-sm px-5 py-2 text-center active:bg-blue-hover"
+          onClick={handleSubmit(onSubmit)}
+        >
+          Login
+        </button>
+        <p className="text-sm font-light text-center text-text-grey">
+          Don't have an account yet?{" "}
+          <Link
+            to="/register"
+            className="font-medium mx-1 text-blue text-primary-500 hover:underline "
+          >
+            Register
+          </Link>
+        </p>
+      </form>
     </section>
   );
 }
