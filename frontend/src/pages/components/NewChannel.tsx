afc9ce28c82415854a3b860eb2355c75e2dcd4d3
Nurdin Beta <nurdinbeta@gmail.com>
2023-08-02 18:53:17 +0800

    add handle form empty at create new channel

--- frontend/src/pages/components/NewChannel.tsx
+++ frontend/src/pages/components/NewChannel.tsx
@@ -1,5 +1,8 @@
-import { useCallback, useState } from "react";
+import { useState } from "react";
 import AddIcon from "../../assets/add-icon.svg";
+import * as yup from "yup";
+import { Controller, useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
 
 const BACKEND_URL =
   "https://w24-group-final-group-3-production.up.railway.app/";
@@ -11,51 +14,41 @@ interface RequestOption {
   redirect: "follow";
 }
 
+interface ChannelData {
+  name: string;
+  description: string;
+  imageURL: string;
+}
+
+const schema = yup.object({
+  name: yup.string().required("Channel name is required"),
+  description: yup.string().required("Channel description is required"),
+  imageURL: yup.string().required("Image URL is required"),
+});
+
 export default function NewChannel() {
   const access_token = localStorage.getItem("access_token");
 
   const [newChannelModalIsOpen, setNewChannelModalIsOpen] = useState(false);
-  const [nameInput, setNameInput] = useState("");
-  const [descriptionInput, setDescriptionInput] = useState("");
-  const [urlInput, setUrlInput] = useState("");
 
-  const handleNameInputChange = useCallback(
-    (event: React.ChangeEvent<HTMLInputElement>) => {
-      setNameInput(event.target.value);
-    },
-    []
-  );
-  const handleDescriptionInputChange = useCallback(
-    (event: React.ChangeEvent<HTMLTextAreaElement>) => {
-      setDescriptionInput(event.target.value);
-    },
-    []
-  );
-  const handleUrlInputChange = useCallback(
-    (event: React.ChangeEvent<HTMLInputElement>) => {
-      setUrlInput(event.target.value);
-    },
-    []
-  );
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<ChannelData>({
+    resolver: yupResolver(schema),
+  });
 
-  const handleFormSubmit = (e: React.SyntheticEvent) => {
-    e.preventDefault();
-
+  const handleFormSubmit = (data: ChannelData) => {
     const myHeaders = {
       "Content-Type": "application/json",
       authorization: `Bearer ${access_token}`,
     };
 
-    const raw = JSON.stringify({
-      name: nameInput,
-      description: descriptionInput,
-      imageURL: urlInput,
-    });
-
     const requestOptions: RequestOption = {
       method: "POST",
       headers: myHeaders,
-      body: raw,
+      body: JSON.stringify(data),
       redirect: "follow",
     };
 
@@ -82,32 +75,71 @@ export default function NewChannel() {
           <form
             onClick={(e) => e.stopPropagation()}
             className="bg-very-dark-grey w-[650px] p-10 flex flex-col items-start gap-6 rounded-[24px]"
-            onSubmit={handleFormSubmit}
+            onSubmit={handleSubmit(handleFormSubmit)}
           >
             <h3 className="text-white text-body-bold">NEW CHANNEL</h3>
-            <input
-              type="text"
-              placeholder="Channel name"
-              className="bg-light-grey h-[48px] p-4 rounded-md outline-none w-full text-white placeholder:text-text-grey text-input-medium placeholder:text-input-medium"
-              onChange={handleNameInputChange}
+            <Controller
+              name="name"
+              control={control}
+              render={({ field }) => (
+                <>
+                  <input
+                    type="text"
+                    {...field}
+                    placeholder="Channel name"
+                    className={`bg-light-grey h-[48px] p-4 rounded-md outline-none w-full text-white ${
+                      errors?.name && "border-red"
+                    } placeholder:text-text-grey text-input-medium placeholder:text-input-medium`}
+                  />
+                  {errors?.name && (
+                    <p className="text-sm text-red text-input-medium">
+                      {errors.name.message}
+                    </p>
+                  )}
+                </>
+              )}
             />
-            <textarea
+            <Controller
               name="description"
-              id="description"
-              placeholder="Channel description"
-              cols={30}
-              rows={4}
-              className="bg-light-grey p-4 rounded-md outline-none w-full text-white placeholder:text-text-grey text-input-medium placeholder:text-input-medium resize-none"
-              onChange={handleDescriptionInputChange}
-            ></textarea>
-            <input
-              type="text"
-              placeholder="Channel image url"
-              className="bg-light-grey h-[48px] p-4 rounded-md outline-none w-full text-white placeholder:text-text-grey text-input-medium placeholder:text-input-medium"
-              onChange={handleUrlInputChange}
+              control={control}
+              render={({ field }) => (
+                <>
+                  <textarea
+                    {...field}
+                    placeholder="Channel description"
+                    cols={30}
+                    rows={4}
+                    className="bg-light-grey p-4 rounded-md outline-none w-full text-white placeholder:text-text-grey text-input-medium placeholder:text-input-medium resize-none"
+                  />
+                  {errors?.description && (
+                    <p className="text-sm text-red text-input-medium">
+                      {errors.description.message}
+                    </p>
+                  )}
+                </>
+              )}
+            />
+            <Controller
+              name="imageURL"
+              control={control}
+              render={({ field }) => (
+                <>
+                  <input
+                    type="text"
+                    {...field}
+                    placeholder="Channel image url"
+                    className="bg-light-grey h-[48px] p-4 rounded-md outline-none w-full text-white placeholder:text-text-grey text-input-medium placeholder:text-input-medium"
+                  />
+                  {errors?.imageURL && (
+                    <p className="text-sm text-red text-input-medium">
+                      {errors.imageURL.message}
+                    </p>
+                  )}
+                </>
+              )}
             />
             <button
-              onClick={handleFormSubmit}
+              type="submit"
               className="bg-blue py-1 px-6 rounded-lg text-white text-body-medium ml-auto active:bg-blue-hover outline-none"
             >
               Save
